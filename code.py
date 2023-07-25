e3ba6265edbb3a10e5ba771e07e5cda666d7767c
Wilyanse <willjose51@gmail.com>
2023-07-25 15:15:10 +0800

    Test PandaSQL module
    Using PandaSQL, I was able to perform SQL queries despite not having any SQL based systems installed.

--- code.py
+++ code.py
@@ -3,6 +3,25 @@ import pandasql as ps
 
 path = r'E:\Code\anino_exam_dataeng\AninoDataEngineerExam\dataengineering-test-data.csv'
 
+cache = {}
+
+def cached_date_parser(s):
+    if s in cache:
+        return cache[s]
+    dt = pd.to_datetime(s, format='%Y%m%d', errors='coerce')
+    cache[s] = dt
+    return dt
+    
 df = pd.read_csv(path)
+df['install_date'] = pd.to_datetime(df['install_date'])
+df['event_time'] = pd.to_datetime(df['event_time'])
 
 print(df.shape)
+print(df.dtypes)
+
+pysqldf = lambda q: ps.sqldf(q, globals())
+
+print(pysqldf('''
+    SELECT COUNT(DISTINCT event_user)
+   FROM df
+'''))
